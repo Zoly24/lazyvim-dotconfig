5503ad3ca6817ca4912af3cd4d5cacdb30633d9d
Zoly24 <alexzanderwilburn2424@outlook.com>
2025-06-02 13:11:56 -0500

    all  other stuff

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "rmehri01/onenord.nvim",
+    name = "onenord",
+    config = function()
+      vim.cmd("colorscheme onenord")
+    end,
+  },
+}
---
+++ lua/plugins/transparent.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "tribela/transparent.nvim",
+    event = "VimEnter",
+    config = true,
+    extra_groups = {
+      "NormalFloat",
+      "NvimTreeNormal",
+    },
+  },
+}
